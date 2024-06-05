2d043a2752bd6376c7fed2c48235ef84c18eafd6
Alfred Gui <alfredzqgui@gmail.com>
2024-06-05 05:07:31 -0400

    add phi model

--- server/text_generation_server/models/flashinfer_causal_lm.py
+++ server/text_generation_server/models/flashinfer_causal_lm.py
@@ -7,14 +7,18 @@ from typing import Any, TypedDict, Optional
 from transformers.models.llama.modeling_llama import LlamaConfig
 from text_generation_server.utils.lora_utils import ModelLoraManager, ModelConfigForLora
 from text_generation_server.utils.cache_manager_flashinfer import ModelKvCache, KvCachePool
-from .custom_modeling.flashinfer_llama_modeling import LlamaForCausalLM
-from .custom_modeling.flashinfer_gemma_modeling import (
+from text_generation_server.models.custom_modeling.flashinfer_llama_modeling import LlamaForCausalLM
+from text_generation_server.models.custom_modeling.flashinfer_gemma_modeling import (
     GemmaTokenizerFast,
     FlashGemmaForCausalLM,
     GemmaConfig,
 )
+from text_generation_server.models.custom_modeling.flashinfer_phi_modeling import (
+    FlashPhiForCausalLM,
+    PhiConfig,
+)
 from .custom_modeling.flashinfer_mistral_modeling import MistralConfig, FlashMistralForCausalLM
-from transformers import PreTrainedTokenizerBase
+from transformers import PreTrainedTokenizerBase, AutoTokenizer
 import transformers
 from text_generation_server.pb import generate_pb2
 import json
@@ -38,7 +42,6 @@ from text_generation_server.utils import (
 )
 from text_generation_server.utils.dist import MEMORY_FRACTION
 from dataclasses import dataclass
-from transformers import AutoTokenizer
 
 from loguru import logger
 tracer = trace.get_tracer(__name__)
@@ -327,6 +330,68 @@ class FlashinferLM(Model):
             model = FlashMistralForCausalLM(None, mistralConfig, weights)
             tokenizer = AutoTokenizer.from_pretrained(model_id)
             model.config = mistralConfig
+        elif model_type == "phi":
+            self.process_group, rank, world_size = initialize_torch_distributed()
+            if torch.cuda.is_available():
+                device = torch.device(f"cuda:{rank}")
+                dtype = torch.float16 if dtype is None else dtype
+            else:
+                raise NotImplementedError("FlashPhi is only available on GPU")
+
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_id,
+                revision=revision,
+                padding_side="left",
+                truncation_side="left",
+                trust_remote_code=trust_remote_code,
+            )
+
+            config = PhiConfig.from_pretrained(
+                model_id, revision=revision, trust_remote_code=trust_remote_code
+            )
+            config.quantize = quantize
+            config.speculator = speculator
+
+            torch.distributed.barrier(group=self.process_group)
+
+            filenames = weight_files(model_id, revision=revision, extension=".safetensors")
+            weights = Weights(filenames, device, dtype, process_group=self.process_group)
+            if config.quantize in ["gptq", "awq"]:
+                weights._set_gptq_params(model_id, revision)
+
+            model = FlashPhiForCausalLM(config, weights)
+            if speculator:
+                from text_generation_server.layers.medusa import MedusaModel
+                from huggingface_hub import hf_hub_download
+                import json
+                import os
+                from pathlib import Path
+
+                is_local_model = (
+                    Path(speculator).exists() and Path(speculator).is_dir()
+                ) or os.getenv("WEIGHTS_CACHE_OVERRIDE", None) is not None
+
+                if not is_local_model:
+                    medusa_config = hf_hub_download(
+                        speculator, revision=revision, filename="config.json"
+                    )
+                    medusa_head = hf_hub_download(
+                        speculator, revision=revision, filename="medusa_lm_head.pt"
+                    )
+                else:
+                    medusa_config = str(Path(speculator) / "config.json")
+                    medusa_head = str(Path(speculator) / "medusa_lm_head.pt")
+
+                with open(medusa_config, "r") as f:
+                    config = json.load(f)
+                medusa_sf = medusa_head[: -len(".pt")] + ".safetensors"
+                weights = Weights(
+                    [medusa_sf], device, dtype, process_group=self.process_group
+                )
+                lm_head = model.lm_head
+                model.lm_head = MedusaModel(config, weights, lm_head)
+
+            torch.distributed.barrier(group=self.process_group)            
         else:
             raise NotImplementedError(f"Flashinfer is not implemented for: {model_type}")     
         
