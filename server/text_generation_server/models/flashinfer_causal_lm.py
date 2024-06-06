9d46d660e7f7536b0453bb4c3c7d570855c5169f
Alfred Gui <alfredzqgui@gmail.com>
2024-06-06 21:37:53 +0000

    workarounds for phi

--- server/text_generation_server/models/flashinfer_causal_lm.py
+++ server/text_generation_server/models/flashinfer_causal_lm.py
@@ -17,7 +17,7 @@ from text_generation_server.models.custom_modeling.flashinfer_phi_modeling impor
     FlashPhiForCausalLM,
     PhiConfig,
 )
-from .custom_modeling.flashinfer_mistral_modeling import MistralConfig, FlashMistralForCausalLM
+# from .custom_modeling.flashinfer_mistral_modeling import MistralConfig, FlashMistralForCausalLM
 from transformers import PreTrainedTokenizerBase, AutoTokenizer
 import transformers
 from text_generation_server.pb import generate_pb2
@@ -433,7 +433,7 @@ class FlashinferLM(Model):
         free_memory = max(
             0, total_free_memory - (1 - MEMORY_FRACTION) * total_gpu_memory
         )
-        num_pages_to_allocate = int(free_memory * 0.80 / cache_page_size)
+        num_pages_to_allocate = 200 #int(free_memory * 0.80 / cache_page_size)
         print(f"Cache allocation:\n"
             f"  Cache Page Size: {cache_page_size / 1024 / 1024} MB\n"
             f"  Dtype Size: {dtype_size}\n"
