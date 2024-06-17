43c5c22d82cbdd67465d5f166a4fdb72ece42adb
Yao Lu <fdyaolu@gmail.com>
2024-06-17 10:01:44 -0700

    minor fix in output example

--- server/examples/test_local_api.py
+++ server/examples/test_local_api.py
@@ -19,9 +19,9 @@ if len(sys.argv) == 2:
     test = sys.argv[1]
 else:
     # test = "gemma"
-    test = "llama-3"
+    # test = "llama-3"
     # test = 'llama-3-70'
-    # test = 'llama-2'
+    test = "llama-2"
     # test = 'mistral'
     # test = 'qwen2'
     # test = 'qwen2-1.8'
@@ -270,11 +270,13 @@ while True:
     generations, _, _ = service.generate_token(FlashinferBatch.Empty(batch.id))
     for gen in generations:
         if gen.prefill_tokens:
-            prompt = tokenizer.decode(gen.prefill_tokens.token_ids)
-        if gen.generated_text:
             display_results[gen.request_id] = [
-                "Prompt:\n" + prompt + "\nAnswer:\n" + gen.generated_text.text
+                "Prompt:\n"
+                + tokenizer.decode(gen.prefill_tokens.token_ids)
+                + "\nAnswer:\n"
             ]
+        if gen.generated_text:
+            display_results[gen.request_id] += [gen.generated_text.text]
     # Stop if all input generations are done
     if all([g.generated_text for g in generations]):
         break
