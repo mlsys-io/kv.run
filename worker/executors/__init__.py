25be6054e3362b7c46d89dc6c5268600604eddd8
maple-zhou <zhoulingfeng@sjtu.edu.cn>
2025-09-30 01:29:41 +0800

    fix(pkg): use relative imports in worker.executors.__init__ to work under module execution (torchrun) and direct script runs

--- worker/executors/__init__.py
+++ worker/executors/__init__.py
@@ -1,11 +1,11 @@
-from executors.vllm_executor import VLLMExecutor
-from executors.ppo_executor import PPOExecutor
-from executors.dpo_executor import DPOExecutor
-from executors.sft_executor import SFTExecutor
-from executors.lora_sft_executor import LoRASFTExecutor
-from executors.transformers_executor import HFTransformersExecutor
-from executors.rag_executor import RAGExecutor
-from executors.agent_executor import AgentExecutor
+from .vllm_executor import VLLMExecutor
+from .ppo_executor import PPOExecutor
+from .dpo_executor import DPOExecutor
+from .sft_executor import SFTExecutor
+from .lora_sft_executor import LoRASFTExecutor
+from .transformers_executor import HFTransformersExecutor
+from .rag_executor import RAGExecutor
+from .agent_executor import AgentExecutor
 
 __all__ = [
     "VLLMExecutor",
